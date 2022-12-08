dcc9541e25d2c990fa9fcdf9271b1e35b79462bc
Darien O. Davis <dariendarien03@gmail.com>
2022-12-08 18:12:11 -0500

    new landing page

--- animalproject/saveaspecies/urls.py
+++ animalproject/saveaspecies/urls.py
@@ -4,4 +4,5 @@ from saveaspecies.views import *
 
 urlpatterns = [
     path("home/", Homeview.as_view() , name="home"),
+    path("", Landdingpageview.as_view() , name="landingpage"),
 ]
--- animalproject/saveaspecies/views.py
+++ animalproject/saveaspecies/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 
+
+class Landdingpageview(View):
+    def get(self, request):
+        return render( request=request , template_name = "saveaspecies/landingpage.html", context = {})
+
 class Homeview(View):
     def get(self, request):
         return render( request=request , template_name = "saveaspecies/home.html", context = {})
