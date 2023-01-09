e59cb9faf75452cb68dc26e2222e779bd960dcaa
Darien O. Davis <dariendarien03@gmail.com>
2023-01-09 14:04:01 -0500

    Connect all pages to views and url

--- animalproject/saveaspecies/urls.py
+++ animalproject/saveaspecies/urls.py
@@ -5,5 +5,8 @@ from saveaspecies.views import *
 
 
 urlpatterns = [
-    path("home/", Homeview.as_view() , name="home"), path("", Landingview.as_view() , name="landing"),
+    path("home/", Homeview.as_view() , name="home"), 
+    path("", Landingview.as_view() , name="landing"),
+    path("animalspecies/  ", Animalspeciesview.as_view() , name="species"),
+    path("animaltype/", Animalspeciesview.as_view() , name="species"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
